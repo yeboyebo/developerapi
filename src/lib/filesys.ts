416ff993b239bd2c04cff15c507e2dd6a536933e
xavikortes <javiercortesveliz@gmail.com>
2021-05-06 11:28:34 +0200

    New filesys functions

--- src/lib/filesys.ts
+++ src/lib/filesys.ts
@@ -1,17 +1,24 @@
 import fs from 'fs'
+import fse from 'fs-extra'
 
-const listDirectory = (dirName: string) => {
-  return fs.readdirSync(dirName)
-}
-const readFile = (file: string) => {
-  return fs.readFileSync(file, 'utf8')
-}
-const writeFile = (file: string, params: any) => {
-  return fs.writeFileSync(file, params, 'utf8')
+const listDirectory = (dirName: string) => fs.readdirSync(dirName)
+const createDirectory = (dir: string) => fs.mkdirSync(dir, { recursive: true })
+const readFile = (file: string) => fs.readFileSync(file, 'utf8')
+const writeFile = (file: string, content: string) => fs.writeFileSync(file, content, 'utf8')
+const copyFile = (source: string, dest: string) => fse.copySync(source, dest)
+const deleteFile = (file: string) => fs.unlinkSync(file)
+const replaceInFile = (file: string, replaceArgs: string[][]) => {
+  let content = readFile(file)
+  replaceArgs.map(arg => content = content.replace(arg[0], arg[1]))
+  return writeFile(file, content)
 }
 
 export default {
+  createDirectory,
   listDirectory,
   readFile,
-  writeFile
+  writeFile,
+  copyFile,
+  replaceInFile,
+  deleteFile
 }
