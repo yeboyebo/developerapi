7057e2983f622ab0fccd651c64ee3a395ecbd618
xavikortes <javiercortesveliz@gmail.com>
2021-05-06 11:28:56 +0200

    Module creation

--- src/lib/moduleParser.ts
+++ src/lib/moduleParser.ts
@@ -9,10 +9,10 @@ const _listModule = (fixed: string, path?: string): string[] => {
 }
 
 const _readIndex = (path: string) => {
-  let stdout = filesys.readFile(`${path}/index.js`)
-  stdout = stdout.split('export default')[1].replace(/\'/g, '"')
-  stdout = stdout.replace(/([^\"\w+:])(\w+):/gm, '$1"$2\":')
-  return JSON.parse(stdout)
+  let content = filesys.readFile(`${path}/index.js`)
+  content = content.split('export default')[1].replace(/\'/g, '"')
+  content = content.replace(/([^\"\w+:])(\w+):/gm, '$1"$2\":')
+  return JSON.parse(content)
 }
 
 const _writeIndex = (path: string, params: any) => {
@@ -23,8 +23,28 @@ const _writeIndex = (path: string, params: any) => {
   return {pk: params.path}
 }
 
+const _createModule = (name: string, dir: string, template: string) => {
+  filesys.createDirectory(`${dir}/${name.split('/').slice(0, -1).join('/')}`)
+  filesys.copyFile(template, `${dir}/${name}`)
+}
+
+const _createProject = (name: string, dir: string, template: string) => {
+  _createModule(name, dir, template)
+  filesys.replaceInFile(`${dir}/${name}/index.js`, [['__template__', name]])
+  return {pk: name}
+}
+
+const _createExtension = (name: string, dir: string, template: string) => {
+  _createModule(name, dir, template)
+  filesys.replaceInFile(`${dir}/${name}/index.js`, [['__template__', name]])
+  filesys.deleteFile(`${dir}/${name}/views/.tmp`)
+  return {pk: dir}
+}
+
 export default {
   listModule: _listModule,
   readIndex: _readIndex,
-  writeIndex: _writeIndex
+  writeIndex: _writeIndex,
+  createProject: _createProject,
+  createExtension: _createExtension
 }
